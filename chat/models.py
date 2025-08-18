d6ee2ae159da4e7bb862c904e1e1757145292995
rrinny <r_r_i_n@naver.com>
2025-08-18 16:02:04 +0900

    feat: chat_mode 필드 수정

--- chat/models.py
+++ chat/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from match.models import UserMatchPreference
+
 User = get_user_model()
 
 class ChatRoom(models.Model):
     requester = models.ForeignKey(User, related_name="requested_chatrooms", on_delete=models.CASCADE)  # 요청자
     receiver = models.ForeignKey(User, related_name="received_chatrooms", on_delete=models.CASCADE)  # 수신자
-    chat_mode = models.CharField(max_length=20) # 생성 시점 모드
+    chat_mode = models.PositiveSmallIntegerField(default=UserMatchPreference.MODE_BUDDY) # 생성 시점 모드
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)  # 대화 요청 수락 시 True
 
