a23c1e36ae9ae8d7307bc1e02622c5ad88ea6e5e
rrinny <r_r_i_n@naver.com>
2025-08-20 17:38:29 +0900

    config: 프론트엔드 테스트용 CORS 설정 수정

--- config/settings.py
+++ config/settings.py
@@ -220,4 +220,9 @@ REST_FRAMEWORK = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
+CORS_ALLOW_CREDENTIALS = True
